91ed07bf89d6fd5b3fc585f62bd2185e9596bb3a
Usaid Asif <usaidasif12@gmail.com>
2023-04-10 04:40:53 +0500

    Created app bar

--- lib/Screens/home_screen.dart
+++ lib/Screens/home_screen.dart
@@ -10,6 +10,54 @@ class HomeScreen extends StatefulWidget {
 class _HomeScreenState extends State<HomeScreen> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return DefaultTabController(
+      length: 3,
+      child: Scaffold(
+        appBar: AppBar(
+          backgroundColor: const Color(0xFF008069),
+          automaticallyImplyLeading: false,
+          title: const Text(
+            "WhatsApp",
+            style: TextStyle(
+              letterSpacing: 1,
+            ),
+          ),
+          elevation: 1,
+          actions: [
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.search,
+              ),
+            ),
+            IconButton(
+              onPressed: () {},
+              icon: const Icon(
+                Icons.more_vert,
+              ),
+            ),
+          ],
+          bottom: const TabBar(
+            indicatorWeight: 3,
+            labelStyle: TextStyle(fontWeight: FontWeight.bold),
+            splashFactory: NoSplash.splashFactory,
+            tabs: [
+              Tab(
+                text: ('Chat'),
+              ),
+              Tab(
+                text: ('Status'),
+              ),
+              Tab(
+                text: ('Calls'),
+              ),
+            ],
+          ),
+        ),
+        body: const TabBarView(
+          children: [],
+        ),
+      ),
+    );
   }
 }
